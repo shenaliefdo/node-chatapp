7a3c002e8462e98acc693f20fb720c238037200a
shenaliefdo <shenalie.fdo@gmail.com>
2018-08-07 13:30:54 -0400

    socket connection between client and server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const path_plan = path.join(__dirname,'../public');
 //console.log(path_plan);
@@ -7,13 +9,24 @@ const path_plan = path.join(__dirname,'../public');
 var port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app); 
+var io = socketIO(server);  
 
 //server up middleware access to public
 app.use(express.static(path_plan));
 
+io.on('connection',function(socket){
+    console.log('new user connection');  
+    
+    socket.on('disconnect',function(socket){
+        console.log('user disconnected');
+    });
+});
+
+
 
 //listen on port 3000
-app.listen(port,function(){
+server.listen(port,function(){
     console.log(`server is up on port ${port}`);
 });
 
